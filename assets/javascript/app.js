93e3a67cfebfea9fcef80a235965e6da003e539f
Erin Hancock <welljer@gmail.com>
2018-11-27 18:32:07 -0700

    third push

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+// Questions in a object //
+
+// function for countdown timer //
+
+// loop through correctarray to match html elements and answers//
+
+// display incorrect answers//
+
+// alert ("time is up")//
+
+// click event for submit button//
+
+// end countdown//
+
+//function to grade quiz//
+
+//once submit is clicked test stops timer//
+
