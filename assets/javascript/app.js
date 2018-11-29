7f5b7940a4fe3149dee20d97eb635d7867ada4f3
Erin Hancock <welljer@gmail.com>
2018-11-28 17:41:25 -0700

    third push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,76 @@
 // Questions in a object //
+var questions = [
+{
+    question:"Which famous toy manufacturer is also the world’s largest tire manufacturer by units produced?",
+    answer:["lego","hasbro","mattel","bandai"],
+    name: "tireManufacturer",
+    correct: "lego",
+    divClass: ".tireManufacturer"
+},
+{
+    question:"In which state was the first oil well drilled in the United States?",
+    answer: ["North Carolina","Texas","Pennsylvania","Tennessee"],
+    name: "oilWell",
+    correct: "Pennsylvania",
+    divClass: ".oilWell",
+},
+{
+    question:"How many letters are in the Greek alphabet?",
+    answer: ["18","24","26","22"],
+    name: "greek",
+    correct: "24",
+    divClass: ".greek"
+},
+{
+    question:"Who was the first queen of England?",
+    answer: ["Elizabeth I","Anne","Emma","Mary I"],
+    name: "queen",
+    correct: "Mary I",
+    divClass: ".queen"
+},
+{
+    question:"How many moons does the planet Venus have?",
+    answer:["4","0","1","2"],
+    name: "moons",
+    correct: "0",
+    divClass: ".moons"
+},
+{
+    question:"Which country lies on the border between Spain and France?",
+    answer:["Morocco","Andorra","Italy","Switzerland"],
+    name: "country",
+    correct: "Andorra",
+    divClass:".country",
+},
+{
+    question:"What is the most abundant element in the earth’s atmosphere?",
+    answer:["Nitrogen","Oxygen","Hydrogen","Ethanol"],
+    name: "element",
+    correct: "Nitrogen",
+    divClass:".element",
+},
+{
+    question:"The largest volcano ever discovered in our solar system is located on which planet?",
+    answer:["Earth","Jupiter","Pluto","Mars"],
+    name: "volcano",
+    correct: "Mars",
+    divClass: ".volcano"
+},
+{
+    question:"The island of Saipan is a commonwealth of which country?",
+    answer:["The United States","Canada","Japan","United Kingdom"],
+    name: "island",
+    correct: "The United States",
+    divClass: ".island",
+},
+{
+    question:"Su Lin was the name given to what type of animal captured in China and brought to the United States for the first time in 1936?",
+    answer:["Flying Fox","Giant Panda","Bengal Tiger","Macaque"],
+    name:"animal",
+    correct:"Giant Panda",
+    divClass:".animal",
+},
+]
 
 // function for countdown timer //
 
